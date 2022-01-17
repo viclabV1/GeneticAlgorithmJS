139f8f86508a49dabeaf002df86292fc971c961d
Victor LaBrie <vplabrie@gmail.com>
2022-01-16 18:35:13 -0600

    Basic genetic algorithm works.

--- geneticAlgo3d/main.js
+++ geneticAlgo3d/main.js
@@ -12,7 +12,7 @@ import { Vector3 } from 'three';
 //SETTING UP SCENE
 //
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 0.1, 300);
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 300);
 const renderer = new THREE.WebGLRenderer({canvas: document.querySelector("#mainCanvas")});
 const controls = new OrbitControls(camera, renderer.domElement);
 const light = new THREE.AmbientLight(0XFFFFFF);
@@ -26,7 +26,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 controls.autoRotate=true;
 controls.autoRotateSpeed=1.0;
-controls.target.set(5,5,5);
+controls.target.set(10,10,10);
 
 scene.add(light);
 
@@ -57,7 +57,7 @@ scene.add(line);
 const goalGeo = new THREE.BoxGeometry(1,1,1);
 const goalMat = new THREE.MeshStandardMaterial({color: 0XFF0000});
 const goal = new THREE.Mesh(goalGeo, goalMat);
-goal.position.set(10,10,10);
+goal.position.set(20,20,20);
 scene.add(goal);
 
 //
@@ -67,13 +67,14 @@ let motherGenes=[];
 let fatherGenes=[];
 let generation=0;
 let agents = [];
+let agentNumber = 50
 
 //function for creating generation
 function newGeneration(){
-  //agents = [];
-  for(let i = 0; i<25; i ++ ){
+  for(let i = 0; i<agentNumber; i ++ ){
     addAgent();
   }
+
 }
 
 class agent{
@@ -90,11 +91,38 @@ function addAgent(){
   let agentMesh = new THREE.Mesh(agentGeometry, agentMaterial);
   agentMesh.position.set(0,0,0);
   const genes = [];
-  
-  for(let i = 0; i<20; i++){
-    genes.push(new THREE.Vector3(THREE.MathUtils.randFloat(-1,1),THREE.MathUtils.randFloat(-1,1),THREE.MathUtils.randFloat(-1,1)));
+  let mutationX=0;
+  let mutationY=0;
+  let mutationZ=0;
+  let mutationVector=new THREE.Vector3(0,0,0);
+  let mutatedVector=new THREE.Vector3(0,0,0);
+  let mutationRate=.5;
+  if(generation<1){
+    for(let i = 0; i<20; i++){
+      genes.push(new THREE.Vector3(THREE.MathUtils.randFloat(-1,1),THREE.MathUtils.randFloat(-1,1),THREE.MathUtils.randFloat(-1,1)));
+    }
   }
-  
+  else{
+    for(let i = 0; i<20; i++){
+      if(i<10){
+      mutationX=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
+      mutationY=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
+      mutationZ=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
+      mutationVector=new THREE.Vector3(mutationX,mutationY,mutationZ);
+      mutatedVector=mutationVector.add(motherGenes[i]);
+      genes.push(mutatedVector);
+      }
+      else{
+      mutationX=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
+      mutationY=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
+      mutationZ=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
+      mutationVector=new THREE.Vector3(mutationX,mutationY,mutationZ);
+      mutatedVector=mutationVector.add(fatherGenes[i]);
+      genes.push(mutatedVector);
+      }
+      
+    }
+  }
   const thisAgent=new agent(agentMesh, genes);
 
   scene.add(thisAgent.mesh);
@@ -102,28 +130,11 @@ function addAgent(){
 }
 
 
-//called for every subsequent generation
-function reproduction(){
-  let reproducedGenes=[];
-  let mutationRate=0.1;
-  for(let i = 0; i < 20; i++){
-    let mutation=THREE.MathUtils.randFloat(-mutationRate,mutationRate);
-    if(i<10){
-      reproducedGenes.push(motherGenes[i]+mutation);
-    }
-    else{
-      reproducedGenes.push(fatherGenes[i]+mutation);
-    }
-  }
-  return reproducedGenes;
-}
 
 //calculates which agents were
 //the first and second most fit, makes them mother and father
 let fittestDone=false;
 function survivalOfTheFittest(){
-  const fittestMaterial = new THREE.MeshStandardMaterial({color: 0X00FF00});
-  const agentGeometry = new THREE.SphereGeometry(0.2,10,10);
   const zeroVector = new THREE.Vector3(0,0,0);
   const maxDistance=zeroVector.distanceTo(goal.position);
   let fitness=0;
@@ -132,7 +143,7 @@ function survivalOfTheFittest(){
   let secondFittest=0;
   let secondFittestIndex=0;
   let thisDistance=0;
-  for(let i = 0; i<25; i ++ ){
+  for(let i = 0; i<agentNumber; i ++ ){
     thisDistance=agents[i].mesh.position.distanceTo(goal.position);
     fitness = 1-(thisDistance/maxDistance);
     if(fitness>secondFittest){
@@ -145,7 +156,7 @@ function survivalOfTheFittest(){
       fittest=fitness;
       fittestIndex=i;
     }
-    console.log(fitness);
+    //console.log(fitness);
   }
   agents[fittestIndex].mesh.material.color.set(0X00FF00);
   agents[secondFittestIndex].mesh.material.color.set(0X00FF00);
@@ -153,8 +164,8 @@ function survivalOfTheFittest(){
   motherGenes=agents[fittestIndex].genes;
   fatherGenes=agents[secondFittestIndex].genes;
 
-  console.log('fittest', fittestIndex, ' ', fittest );
-  console.log('2ndfittest', secondFittestIndex, ' ', secondFittest );
+  //console.log('fittest', fittestIndex, ' ', fittest, motherGenes );
+  //console.log('2ndfittest', secondFittestIndex, ' ', secondFittest, fatherGenes );
 
 }
 
@@ -165,7 +176,7 @@ let simulationSpeed=0.08;
 let currentVector=0;
 function runSimulation(){
   currentVector = Math.floor((clock.elapsedTime/10) * 20);
-  for(let i=0; i<25; i++){
+  for(let i=0; i<agentNumber; i++){
     agents[i].mesh.position.x+=(agents[i].genes[currentVector].x*simulationSpeed);
     agents[i].mesh.position.y+=(agents[i].genes[currentVector].y*simulationSpeed);
     agents[i].mesh.position.z+=(agents[i].genes[currentVector].z*simulationSpeed);
@@ -178,9 +189,9 @@ function runSimulation(){
 //
 newGeneration();
 let cleared=false;
-
+let generationExists=true;
 function animate(){
-  if(clock.getElapsedTime()<=10 && currentVector<25){
+  if(clock.getElapsedTime()<=10 && currentVector<agentNumber){
     runSimulation();
   }
   else if(clock.getElapsedTime()<=15 && !fittestDone){
@@ -192,19 +203,30 @@ function animate(){
     //clock.stop();
     //clock.start();
     fittestDone=false;
-    for(let i = 0; i < 25; i++){
+    for(let i = 0; i < agentNumber; i++){
       scene.remove(agents[i].mesh)
     }
+    console.log(motherGenes);
+    console.log(fatherGenes);
     agents = [];
     cleared=true;
-
+    generationExists=false;
   }
+  else if(!generationExists){
+    clock.stop();
+    clock.start();
+    generation++;
+    newGeneration();
+    generationExists=true;
+    console.log('generation: ',generation);
+  }
+
   renderer.setSize(window.innerWidth,window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   camera.aspect=(window.innerWidth/window.innerHeight);
   camera.updateProjectionMatrix();
   renderer.render(scene,camera);
-  camera.lookAt(5,5,5);
+  camera.lookAt(10,10,10);
   controls.update();
   requestAnimationFrame(animate);
 }
